8655aedd33b9c0f25541c8e82e35e69557d14561
Edwing Picado <empireeapp@gmail.com>
2023-09-12 03:37:20 +0200

    fix:prueba webpack prod

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,26 +1,15 @@
 const common = require('./webpack.common')
 const { merge } = require('webpack-merge')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 /** @type {import('webpack').Configuration} */
 const prodConfig = {
   mode: 'production',
-  module: {
-    rules: [
-      {
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      }
-    ]
-  },
   optimization: {
     splitChunks: {
       chunks: 'all',
       name: false
     }
-  },
-  plugins: [
-    new MiniCssExtractPlugin() // para extraer el css
-  ]
+  }
 }
 
 module.exports = merge(common, prodConfig) // mezclamos las configuraciones del webpack.common.js con el webpack.prod.js

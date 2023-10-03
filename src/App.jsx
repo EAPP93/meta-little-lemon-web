51c3ad782ca24acca2d0ad2e757c4467312e0d9a
Edwing Picado <empireeapp@gmail.com>
2023-10-03 10:55:34 +0200

    fix(app.jsx): add global state

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/router'
 import { SizeProvider } from './context/SizeProvider'
+import { DateProvider } from './context/DateProvider'
 export default function App () {
   return (
-    <SizeProvider>
-      <RouterProvider router={router} />
-    </SizeProvider>
+    <DateProvider>
+      <SizeProvider>
+        <RouterProvider router={router} />
+      </SizeProvider>
+    </DateProvider>
   )
 }

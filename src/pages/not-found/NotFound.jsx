14e48d81e25391c29c2600a00e867aeb4f1579b0
Edwing Picado <empireeapp@gmail.com>
2023-10-04 19:57:44 +0200

    fix(notfound): fit and add btn back to home page

--- src/pages/not-found/NotFound.jsx
+++ src/pages/not-found/NotFound.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import Picture from '../../components/picture'
 import styles from './not-found.module.css'
+import { Link } from 'react-router-dom'
 
 export default function NotFound () {
   const img = () => require('../../assets/img/Logo.svg')
   return (
     <main className={styles.NotFound}>
-      <div className={styles['NotFound-container']}>
+      <section className={styles['NotFound-section']}>
+        <div className={styles['NotFound-container']}>
         <Picture defaultImage={{ src: img(), alt: 'image of little lemon' }} list={[]}></Picture>
       </div>
       <div className={styles['NotFound-container']}>
         <h1 className={styles['NotFound-title']}>Error 404</h1>
       </div>
+      </section>
+
+        <Link to='/' className={styles['NotFound-link']}>
+          Back to home page
+        </Link>
     </main>
   )
 }
